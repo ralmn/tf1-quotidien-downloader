d51f10ae729b84731d1a7f8c6ff8565827620a3a
Mathieu "ralmn" HIREL <contact.ralmn@gmail.com>
2016-12-16 22:47:48 +0100

    Command options

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-var http = require('http');
-var exec = require('child_process').exec;
-var fs = require('fs');
+const http = require('http');
+const exec = require('child_process').exec;
+const fs = require('fs');
+const program = require('commander');
+const package = require('./package.json');
+const path = require('path');
+const moment = require('moment');
 
-/* Some config */
-var path = '/path/to/your/download/directory/';
 
+//Commander path
+
+program
+    .version(package.version)  
+    .option('-d --date [date]', 'Date of diffusions', 'none')
+    .option('--date-format [format]', 'Date format', 'YYYY-MM-DD')
+    .option('-p --parts [1er|2eme|all]', 'Parts of diffusions', 'all')
+    .option('-o --out [out]', 'Out directory', '.')
+    .parse(process.argv);
+
+
+
+
+var outDir = program.out;
 
 /* Content */
 function startDL(link) {
@@ -14,9 +30,11 @@ function startDL(link) {
             child = exec("youtube-dl -f mp4 -s --get-filename " + link, function (err, stdout, stderr) {
                 if (!err) {
                     var filename = stdout.replace(/ /g, '_').replace(/,/g, '').replace('-' + fileid, '').replace('\n', '');
-                    fs.access(path + filename, fs.F_OK, function (err) {
+                    var filePath = path.join(outDir, filename);
+                    fs.access(filePath, fs.F_OK, function (err) {
                         if (err != null) {
-                            var cmd = "youtube-dl -f mp4 -o " + path + filename + " " + link;
+                            console.log('Start download of', link);
+                            var cmd = "youtube-dl -f mp4 -o " + filePath + " " + link;
                             child = exec(cmd, {maxBuffer: 1024 * 1000}, function (err, stdout, stderr) {
                                 if (!err) {
                                     console.log(filename + ': downloaded!');
@@ -24,10 +42,14 @@ function startDL(link) {
                                     console.log('ERR#3: ' + err);
                                 }
                             });
+                            child.stdout.on('data', function(data){
+                                process.stdout.write(data);
+                            });
                         } else {
                             console.log(filename + ': already exists!');
                         }
                     });
+
                 } else {
                     console.log('ERR#2: ' + err);
                 }
@@ -36,6 +58,37 @@ function startDL(link) {
             console.log('ERR#1: ' + err);
         }
     });
+}
+
+function extractDate(link){
+    var indexStartDate = link.indexOf('-partie-') + 8;
+    var indexEndDate = link.indexOf('.html');
+
+    var strDate = link.substring(indexStartDate, indexEndDate);
+    return moment(strDate, 'DD-MMMM-YYYY');
+}
+
+function okForDownload(link){
+    var res = true;
+    if(program.date != "none"){
+        var argDate = moment(program.date, program.dateFormat);
+        var linkDate = extractDate(link);
+        if(!argDate.isSame(linkDate)){
+            res = false;
+        }
+    }
+    if(res && program.parts != 'all'){
+        if(program.parts == '1er'){
+            if(link.indexOf('premiere') == -1){
+                res = false
+            }
+        }else{
+            if(link.indexOf('deuxieme') == -1){
+                res = false
+            }
+        }
+    }
+    return res;
 }
 
 var req = http.get({host: 'www.tf1.fr', path: '/tmc/quotidien-avec-yann-barthes/videos?filter=replay'}, function (res) {
@@ -47,9 +100,13 @@ var req = http.get({host: 'www.tf1.fr', path: '/tmc/quotidien-avec-yann-barthes/
         var results = body.match(/<a href="\/tmc\/quotidien-avec-yann-barthes\/videos\/quotidien-(\w*)-partie-(\d*)-(\w*)-(\d*).html" class="link videoLink trackXiti testCSA"/gi);
         var links = [];
         for (var result in results) links.push('http://www.tf1.fr' + results[result].replace('<a href="', '').replace('" class="link videoLink trackXiti testCSA"', ''));
-        for (var link in links) {
-            console.log('Starting DL of ' + links[link]);
-            startDL(links[link]);
+        for (var i in links) {
+            var link = links[i];
+            if(okForDownload(link)){
+                console.log('Init download of', link);
+                startDL(link);
+            }
+
         }
 
     });
